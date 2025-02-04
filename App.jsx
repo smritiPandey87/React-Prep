b86847ad2e4f29190e923bec3aa48351dddcf679
smriti <rajsimmi369@gmail.com>
2025-02-04 17:24:32 +0530

    useState

--- App.jsx
+++ App.jsx
@@ -1,11 +1,25 @@
-//
+import { useState } from "react";
+
+// function App() {
+//   let data = "simmi hye ";
+//   return (
+//     <div className="App">
+//       <h2> {data}</h2>
+//       <button> click me </button>
+//     </div>
+//   );
+// }
+// export default App;
 
 function App() {
-  let data = "simmi hye ";
+  const [data, setData] = useState(0);
+  function updateData() {
+    setData(data+1);
+  }
   return (
     <div className="App">
-      <h2> {data}</h2>
-      <button> click me </button>
+      <h1>{data}</h1>
+      <button onClick={updateData}>update data</button>
     </div>
   );
 }
