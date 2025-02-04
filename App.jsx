765767508ff2bbc68c6a78a4e37129d2a927a1fe
smriti <rajsimmi369@gmail.com>
2025-02-04 16:15:50 +0530

    click Event

--- App.jsx
+++ App.jsx
@@ -1,22 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Users from "./User";
+//
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm a h1 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm a h1 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "function components"),
-    React.createElement(Users),// for using again and again 
-    React.createElement(Users),
-  ]),
-]);
-
-console.log(parent);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
+function App() {
+  let data = "simmi hye ";
+  return (
+    <div className="App">
+      <h2> {data}</h2>
+      <button> click me </button>
+    </div>
+  );
+}
+export default App;
--- User.jsx
+++
@@ -1,8 +0,0 @@
-function Users()
-{
- return (
-    <h1>function Components</h1>
- )
-}
-
-export  default  Users;
